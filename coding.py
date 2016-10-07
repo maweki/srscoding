b7f777e2523b4bded96cbfab84194fbe2a2c4e9f
Mario Wenzel <maweki@gmail.com>
2016-10-07 17:44:11 +0200

    nicer frozenset constructor

--- coding.py
+++ coding.py
@@ -1,28 +1,30 @@
 from sys import argv
 from itertools import product
 
+fs = lambda *e: frozenset(e)
+
 def prefixes(a):
     return frozenset(a[:d+1] for d in range(len(a)))
 
 def suffixes(a):
     return frozenset(a[d:] for d in range(len(a)))
 
-assert prefixes('abc') == frozenset(['a', 'ab', 'abc'])
-assert suffixes('abc') == frozenset(['c', 'bc', 'abc'])
+assert prefixes('abc') == fs('a', 'ab', 'abc')
+assert suffixes('abc') == fs('c', 'bc', 'abc')
 
 words = frozenset(argv[1:])
 
 def coding(w):
     for l, r in product(w, repeat=2):
-        remove = frozenset([l, r])
+        remove = fs(l, r)
         for ovl in suffixes(l) & prefixes(r):
             if ovl == l == r:
                 continue
             if ovl == l:
-                return coding((w - remove) | frozenset([l, r[len(ovl):]]))
+                return coding((w - remove) | fs(l, r[len(ovl):]))
             if ovl == r:
-                return coding((w - remove) | frozenset([r, l[:-len(ovl)]]))
-            return coding(w - remove | frozenset([ovl, r[len(ovl):], l[:-len(ovl)]]))
+                return coding((w - remove) | fs(r, l[:-len(ovl)]))
+            return coding(w - remove | fs(ovl, r[len(ovl):], l[:-len(ovl)]))
     return w
 
 print(coding(words))
