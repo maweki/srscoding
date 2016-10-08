6720bb8f84ec6cc1c2a40673715e630fdd53710f
Mario Wenzel <maweki@gmail.com>
2016-10-08 11:23:04 +0200

    extract vase set

--- coding.py
+++ coding.py
@@ -16,15 +16,15 @@ words = frozenset(argv[1:])
 
 def coding(w):
     for l, r in product(w, repeat=2):
-        remove = fs(l, r)
+        base = w - fs(l, r)
         for ovl in suffixes(l) & prefixes(r):
             if ovl == l == r:
                 continue
             if ovl == l:
-                return coding((w - remove) | fs(l, r[len(ovl):]))
+                return coding(base | fs(l, r[len(ovl):]))
             if ovl == r:
-                return coding((w - remove) | fs(r, l[:-len(ovl)]))
-            return coding(w - remove | fs(ovl, r[len(ovl):], l[:-len(ovl)]))
+                return coding(base | fs(r, l[:-len(ovl)]))
+            return coding(base | fs(ovl, r[len(ovl):], l[:-len(ovl)]))
     return w
 
 print(coding(words))
